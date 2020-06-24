091362b6cf5b5db52b96ed69fa3730bb3574c1a3
wndrlsla <wndrlsla0919@gmail.com>
2020-06-24 17:33:51 +0900

    0624

--- src/main/java/springboard/controller/BbsController.java
+++ src/main/java/springboard/controller/BbsController.java
@@ -1,8 +1,14 @@
 package springboard.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import model.JdbcTemplateConst;
 
 @Controller
 public class BbsController {
@@ -12,7 +18,14 @@ public class BbsController {
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
 		System.out.println("@Autowired->JDBCTemplate 연결성공");
+		
+		JdbcTemplateConst.template = this.template;
 	}
-
+	@RequestMapping("board/list.do")
+	public String list(Model model, HttpServletRequest req) {
+		
+		
+		return "07Board/list";
+	}
 	
 }
